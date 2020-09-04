feb86efaaf116e163f1f1a2c71c3bd190130a6b4
Hoang Quoc Viet <53225445+Kizaruuu@users.noreply.github.com>
2020-09-04 16:01:49 +0700

    fix

--- convert.php
+++ convert.php
@@ -1,35 +1,22 @@
 <?php
 
-
-
 function convertArray(array $arrA, $length)
 {
     $arrB = [];
     for ($i = 0; $i < $length; $i++) {
-
-        if ($length <= count($arrA)) {
-            if ($i == 0) {
-                $arrB[$i] = $arrA[$i] + $arrA[$i + 1];
-            } elseif ($i == ($length - 1)) {
-                $arrB[$i] = $arrA[$i - 1] + $arrA[$i];
-            } else {
-                $arrB[$i] = $arrA[$i - 1] + $arrA[$i] + $arrA[$i + 1];
-            }
-        } else {
-            if ($i == 0) {
-                $arrB[$i] = $arrA[$i] + $arrA[$i + 1];
-            } elseif ( $i == (count($arrA) - 1)) {
-                $arrB[$i] = $arrA[$i - 1] + $arrA[$i];
-            } elseif ($i == count($arrA)) {
-                $arrB[$i] = $arrA[$i - 1];
-            } elseif ($i > count($arrA)) {
-                $arrB[$i] = 0;
-            } else {
-                $arrB[$i] = $arrA[$i - 1] + $arrA[$i] + $arrA[$i + 1];
-            }
-        }
+        $arrB[$i] = checkKey($i - 1, $arrA) + checkKey($i , $arrA) + checkKey($i + 1, $arrA);
     }
 
     return $arrB;
 }
-echo implode(convertArray([4, 0, 1], 5), ', ');
+
+function checkKey($key, array $array)
+{
+    if(array_key_exists($key, $array)) {
+        return $array[$key];
+    }
+
+    return 0;
+}
+
+echo implode(convertArray([1, 2, 3, 4, 5], 5), ', ');
